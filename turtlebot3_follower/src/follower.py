eb33c2b860615159266fd85a4ef63a449b7c6dda
BoGyeong kim <rlaqhrud852@gmail.com>
2017-05-10 17:48:10 +0900

    turtlebot3_application modified

--- turtlebot3_follower/src/follower.py
+++ turtlebot3_follower/src/follower.py
@@ -13,7 +13,7 @@ class follower:
         rospy.loginfo('Follower node initialized')
         self.pub = rospy.Publisher('/turtlebotA/cmd_vel', Twist, queue_size = 1)
         self.clf = pickle.load( open( "clf", "rb")) ## Load classifier
-        self.labels = {'30_0':0, '30_l':1, '30_r':2, '45_0':3, '45_l':4, '45_r':5, 'empty':6}
+        self.labels = {'30_0':0, '30_l':1, '30_r':2, '45_0':3, '45_l':4, '45_r':5,'15_0':6, 'empty':7}
         rospy.loginfo('Tree initialized')
         self.follow()
 
@@ -22,14 +22,17 @@ class follower:
         data_test_set=[]
         self.msg = rospy.wait_for_message("/turtlebotA/scan_filtered", LaserScan)
 
-        for x in range(70,-1,-1) + range(359, 289,-1):
+        for x in range(70,-2,-1) + range(359, 289,-1):
             
             if   np.nan_to_num( self.msg.ranges[x] ) != 0 :
                  data_test.append(np.nan_to_num(self.msg.ranges[x]))
 
-            elif (x+1) in range(70,-1,-1) + range(359, 289,-1) and (x-1) in range(70,-1,-1) + range(359, 289,-1) and np.nan_to_num( self.msg.ranges[x] ) == 0:
+            elif (x+1) in range(70,-2,-1) + range(359, 289,-1) and (x-1) in range(70,-2,-1) + range(359, 289,-1) and np.nan_to_num(self.msg.ranges[x]) == 0:
                  data_test.append((np.nan_to_num(self.msg.ranges[x+1])+np.nan_to_num(self.msg.ranges[x-1]))/2)
                  
+            else :
+                 data_test.append(np.nan_to_num(self.msg.ranges[x]))
+
         data_test_set.append(data_test)
 
         return [x for (x , y) in self.labels.iteritems() if y == self.clf.predict(data_test_set) ] ## Predict the position
@@ -40,17 +43,19 @@ class follower:
             twist = Twist()
             ## Do something according to each position##
             if  x == ['30_0']:
-                twist.linear.x  = 0.02; 	twist.angular.z = 0.0;
+                twist.linear.x  = 0.05; 	twist.angular.z = 0.0;
             elif x== ['30_l']:
-                twist.linear.x  = 0.01; 	twist.angular.z = 0.3;
+                twist.linear.x  = 0.04; 	twist.angular.z = 0.3;
             elif x== ['30_r']:
-                twist.linear.x  = 0.01; 	twist.angular.z = -0.3;
+                twist.linear.x  = 0.04; 	twist.angular.z = -0.3;
             elif x== ['45_0']:
-                twist.linear.x  = 0.04; 	twist.angular.z = 0.0;
+                twist.linear.x  = 0.08; 	twist.angular.z = 0.0;
             elif x== ['45_l']:
-                twist.linear.x  = 0.03; 	twist.angular.z = 0.4;
+                twist.linear.x  = 0.07; 	twist.angular.z = 0.4;
             elif x== ['45_r']:
-                twist.linear.x  = 0.03;	twist.angular.z = -0.4;
+                twist.linear.x  = 0.07;	twist.angular.z = -0.4;
+            elif x== ['15_0']:
+                 twist.linear.x = 0.0; 	twist.angular.z = 0.0; 
             elif x== ['empty']:
                 twist.linear.x  = 0.0; 	twist.angular.z = 0.0;
             else:
--- turtlebot3_follower/src/ploting.py
+++ turtlebot3_follower/src/ploting.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     laser_data = pickle.load( open( "add_data", "rb"))
     comments = pickle.load( open( "add_comment", "rb" ))
-    labels = {'30_0':0, '30_l':1, '30_r':2, '45_0':3, '45_l':4, '45_r':5, 'empty':6}
+    labels = {'30_0':0, '30_l':1, '30_r':2, '45_0':3, '45_l':4, '45_r':5, '15_0':6, 'empty':7}
                                         
 #################################################### WORKING with 0-70 to 290- 360 #############################################
 
@@ -24,38 +24,44 @@ if __name__ == '__main__':
  
     for i in range(len(comments)):
         
-        for x in range(70,-1,-1) + range(359, 289,-1):
+        for x in range(70,-2,-1) + range(359, 289,-1):
             
             if   np.nan_to_num(laser_data[i].ranges[x]) != 0 :
                  data_range_training.append(np.nan_to_num(laser_data[i].ranges[x]))
 
-            elif (x+1) in range(70,-1,-1) + range(359, 289,-1) and (x-1) in range(70,-1,-1) + range(359, 289,-1) and np.nan_to_num(laser_data[i].ranges[x]) == 0:
+            elif (x+1) in range(70,-2,-1) + range(359, 289,-1) and (x-1) in range(70,-2,-1) + range(359, 289,-1) and np.nan_to_num(laser_data[i].ranges[x]) == 0:
                  data_range_training.append((np.nan_to_num(laser_data[i].ranges[x+1])+np.nan_to_num(laser_data[i].ranges[x-1]))/2)
+                 
+            else :
+                 data_range_training.append(np.nan_to_num(laser_data[i].ranges[x]))
         
-        for x in range(70,-1,-1) + range(359, 289,-1):
+        for x in range(70,-2,-1) + range(359, 289,-1):
             
             if   np.nan_to_num(laser_data[i].intensities[x]) != 0 :
                  data_intensity_training.append(np.nan_to_num(laser_data[i].intensities[x]))
             
-            elif (x+1) in range(70,-1,-1) + range(359, 289,-1) and (x-1) in range(70,-1,-1) + range(359, 289,-1) and np.nan_to_num(laser_data[i].intensities[x]) == 0:
+            elif (x+1) in range(70,-2,-1) + range(359, 289,-1) and (x-1) in range(70,-2,-1) + range(359, 289,-1) and np.nan_to_num(laser_data[i].intensities[x]) == 0:
                  data_intensity_training.append((np.nan_to_num(laser_data[i].intensities[x+1])+np.nan_to_num(laser_data[i].intensities[x-1]))/2)             
         
+            else :
+                 data_intensity_training.append(np.nan_to_num(laser_data[i].intensities[x]))
+                 
         data_range_training_set.append(data_range_training)    
         data_intensity_training_set.append(data_intensity_training) 
 
 #################################################### PLOTTING FROM 0-70 290-360 ################################################
-        plt.figure(i)
-        
-        plt.subplot(211)
-        plt.axis([0, 140, 0.0, 0.5])
-        plt.plot(data_range_training, 'b')
-        plt.title(comments[i])
-        plt.ylabel('Range')
-
-        plt.subplot(212)
-        plt.axis([0, 140, 0.0, 8000])
-        plt.plot(data_intensity_training, 'r') 
-        plt.ylabel('Intensitie')        
+#        plt.figure(i)
+#        
+#        plt.subplot(211)
+#        plt.axis([0, 140, 0.0, 0.5])
+#        plt.plot(data_range_training, 'b')
+#        plt.title(comments[i])
+#        plt.ylabel('Range')
+#
+#        plt.subplot(212)
+#        plt.axis([0, 140, 0.0, 8000])
+#        plt.plot(data_intensity_training, 'r') 
+#        plt.ylabel('Intensitie')        
 
         data_range_training = []
         data_intensity_training = []
@@ -69,5 +75,5 @@ if __name__ == '__main__':
     clf = RandomForestClassifier(n_estimators = 10)
     clf.fit(data_range_training_set,Y)
 
-    pickle.dump( clf, open( "clf", "ab"))
+    pickle.dump( clf, open( "clf", "wb"))
 
