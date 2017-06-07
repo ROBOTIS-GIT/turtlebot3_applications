8ff374d5917905a24e763a59052b28f006a4b4dd
Ashe <rlaqhrud852@gmail.com>
2017-06-07 11:15:33 +0900

    turtlebot3 application modified

--- turtlebot3_follower/src/ploting.py
+++ turtlebot3_follower/src/ploting.py
@@ -1,3 +1,35 @@
+# /*******************************************************************************
+# * Copyright (c) 2016, ROBOTIS CO., LTD.
+# * All rights reserved.
+# *
+# * Redistribution and use in source and binary forms, with or without
+# * modification, are permitted provided that the following conditions are met:
+# *
+# * * Redistributions of source code must retain the above copyright notice, this
+# *   list of conditions and the following disclaimer.
+# *
+# * * Redistributions in binary form must reproduce the above copyright notice,
+# *   this list of conditions and the following disclaimer in the documentation
+# *   and/or other materials provided with the distribution.
+# *
+# * * Neither the name of ROBOTIS nor the names of its
+# *   contributors may be used to endorse or promote products derived from
+# *   this software without specific prior written permission.
+# *
+# * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# *******************************************************************************/
+
+# /* Author: Chris, Ashe Kim */
+
 #!/usr/bin/env python
 import pickle
 import rospy
@@ -14,44 +46,44 @@ if __name__ == '__main__':
     laser_data = pickle.load( open( "add_data", "rb"))
     comments = pickle.load( open( "add_comment", "rb" ))
     labels = {'30_0':0, '30_l':1, '30_r':2, '45_0':3, '45_l':4, '45_r':5, '15_0':6, 'empty':7}
-                                        
+
 #################################################### WORKING with 0-70 to 290- 360 #############################################
 
     data_range_training_set = []
     data_range_training = []
     data_intensity_training_set = []
     data_intensity_training = []
- 
+
     for i in range(len(comments)):
-        
+
         for x in range(70,-2,-1) + range(359, 289,-1):
-            
+
             if   np.nan_to_num(laser_data[i].ranges[x]) != 0 :
                  data_range_training.append(np.nan_to_num(laser_data[i].ranges[x]))
 
             elif (x+1) in range(70,-2,-1) + range(359, 289,-1) and (x-1) in range(70,-2,-1) + range(359, 289,-1) and np.nan_to_num(laser_data[i].ranges[x]) == 0:
                  data_range_training.append((np.nan_to_num(laser_data[i].ranges[x+1])+np.nan_to_num(laser_data[i].ranges[x-1]))/2)
-                 
+
             else :
                  data_range_training.append(np.nan_to_num(laser_data[i].ranges[x]))
-        
+
         for x in range(70,-2,-1) + range(359, 289,-1):
-            
+
             if   np.nan_to_num(laser_data[i].intensities[x]) != 0 :
                  data_intensity_training.append(np.nan_to_num(laser_data[i].intensities[x]))
-            
+
             elif (x+1) in range(70,-2,-1) + range(359, 289,-1) and (x-1) in range(70,-2,-1) + range(359, 289,-1) and np.nan_to_num(laser_data[i].intensities[x]) == 0:
-                 data_intensity_training.append((np.nan_to_num(laser_data[i].intensities[x+1])+np.nan_to_num(laser_data[i].intensities[x-1]))/2)             
-        
+                 data_intensity_training.append((np.nan_to_num(laser_data[i].intensities[x+1])+np.nan_to_num(laser_data[i].intensities[x-1]))/2)
+
             else :
                  data_intensity_training.append(np.nan_to_num(laser_data[i].intensities[x]))
-                 
-        data_range_training_set.append(data_range_training)    
-        data_intensity_training_set.append(data_intensity_training) 
+
+        data_range_training_set.append(data_range_training)
+        data_intensity_training_set.append(data_intensity_training)
 
 #################################################### PLOTTING FROM 0-70 290-360 ################################################
 #        plt.figure(i)
-#        
+#
 #        plt.subplot(211)
 #        plt.axis([0, 140, 0.0, 0.5])
 #        plt.plot(data_range_training, 'b')
@@ -60,12 +92,12 @@ if __name__ == '__main__':
 #
 #        plt.subplot(212)
 #        plt.axis([0, 140, 0.0, 4000])
-#        plt.plot(data_intensity_training, 'r') 
-#        plt.ylabel('Intensitie')        
+#        plt.plot(data_intensity_training, 'r')
+#        plt.ylabel('Intensitie')
 
         data_range_training = []
         data_intensity_training = []
-               
+
 #################################################### Random Forest Fitting ####################################################
 
     Y=[]
@@ -80,4 +112,3 @@ if __name__ == '__main__':
 
     pickle.dump( clf, open( "clf", "wb"))
     pickle.dump( clf2, open( "clf2", "wb"))
-
