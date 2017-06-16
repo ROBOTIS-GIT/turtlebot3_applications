1153855ed626f07d7813981579de87b756887e58
Darby Lim <thlim@robotis.com>
2017-06-16 14:21:13 +0900

    update tf

--- turtlebot3_auto_docking/src/get_goal_position.cpp
+++ turtlebot3_auto_docking/src/get_goal_position.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <ros/time.h>
 #include <laser_geometry/laser_geometry.h>
 #include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/PointCloud.h>
@@ -23,7 +24,7 @@ int count = 0;
 float left_array[3] = {0, 0, 0};
 float right_array[3] = {0, 0, 0};
 
-void poseCallback(const sensor_msgs::LaserScan& scan_filtered)
+void poseCallback(const sensor_msgs::LaserScan &scan_filtered)
 {
 //  if(count == 0)
 //  {
@@ -51,24 +52,27 @@ void poseCallback(const sensor_msgs::LaserScan& scan_filtered)
            }
        }
      }
-  //    count = 1;
+      count = 1;
 //  }
-   ts.header.frame_id = "odom";
-   ts.child_frame_id = "final_goal_point";
+   ts.header.stamp = ros::Time::now();
+   ts.header.frame_id = "base_link";
+   ts.child_frame_id = "ts";
    ts.transform.translation.x = final_goal_point.x;
    ts.transform.translation.y = final_goal_point.y;
    ts.transform.translation.z = final_goal_point.z;
+
 
    final_goal_point.x = (left_point.x + right_point.x)/2;
    final_goal_point.y = (left_point.y + right_point.y)/2;
    final_goal_point.z = 0.0;
 
-   check_cloud.publish(cloud);
-   left_point_pub.publish(left_point);
-   right_point_pub.publish(right_point);
-   final_goal_point_pub.publish(final_goal_point);
-   ts_pub.publish(ts);
    ROS_INFO("goal_x %f, goal_y %f ", final_goal_point.x, final_goal_point.y);
+
+//   check_cloud.publish(cloud);
+//   left_point_pub.publish(left_point);
+//   right_point_pub.publish(right_point);
+   ts_pub.publish(ts);
+   final_goal_point_pub.publish(final_goal_point);
 }
 
 int main(int argc, char** argv)
@@ -76,16 +80,18 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "get_goal_position");
   ros::NodeHandle node;
 
-  ros::Rate rate(1000); // 10 hz
+  ros::Rate rate(125); // 10 hz
+
+  left_point_pub = node.advertise<geometry_msgs::Point32>("/left_point", 10);
+  right_point_pub = node.advertise<geometry_msgs::Point32>("/right_point", 10);
+  final_goal_point_pub = node.advertise<geometry_msgs::Point32>("/final_goal_point", 10);
+  check_cloud =  node.advertise<sensor_msgs::PointCloud>("/check_cloud", 10);
+  ts_pub = node.advertise<geometry_msgs::TransformStamped>("/ts", 10);
+
+  laserscan_sub = node.subscribe("/scan", 10, &poseCallback);
+
   while (node.ok())
   {
-    left_point_pub = node.advertise<geometry_msgs::Point32>("/left_point", 10);
-    right_point_pub = node.advertise<geometry_msgs::Point32>("/right_point", 10);
-    final_goal_point_pub = node.advertise<geometry_msgs::Point32>("/final_goal_point", 10);
-    check_cloud =  node.advertise<sensor_msgs::PointCloud>("/check_cloud", 10);
-    ts_pub = node.advertise<geometry_msgs::TransformStamped>("/ts", 10);
-    laserscan_sub = node.subscribe( "/scan_filtered", 10, &poseCallback);
-
     ros::spinOnce();
     rate.sleep();
   }
--- turtlebot3_auto_docking/src/goal_position_broadcaster.cpp
+++ turtlebot3_auto_docking/src/goal_position_broadcaster.cpp
@@ -8,23 +8,9 @@ float goal_x = 0.0, goal_y = 0.0, goal_theta = 0.0;
 
 void final_goal_point_Callback(const geometry_msgs::Point32::ConstPtr& final_goal_point)
 {
-  tf::TransformBroadcaster broadcaster;
-  tf::Transform transform;
-  tf::Quaternion quaternion;
-
   goal_x = final_goal_point->x;
   goal_y = final_goal_point->y;
   goal_theta = atan2(goal_y, goal_x);
-
-  transform.setOrigin( tf::Vector3(goal_x , goal_y , 0.0) );
-  quaternion.setRPY(0, 0, goal_theta);
-  transform.setRotation(quaternion);
-  broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time(0), "odom", "final_goal_point"));
-
-  transform.setOrigin( tf::Vector3(0.2 , 0.2 , 0.0) );
-  quaternion.setRPY(0, 0, 1);
-  transform.setRotation(quaternion);
-  broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time(0), "odom", "test2"));
 
   ROS_INFO("goal_x %f, goal_y %f ", goal_x, goal_y);
 }
@@ -34,7 +20,48 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "goal_position_broadcaster");
   ros::NodeHandle node;
 
+  tf::TransformBroadcaster broadcaster;
+  tf::Transform transform;
+  tf::Quaternion quaternion;
+
   final_goal_point_sub = node.subscribe<geometry_msgs::Point32>( "/final_goal_point", 10, &final_goal_point_Callback);
-  ros::spin();
+
+  ros::Rate rate(125.0);
+  while (node.ok()){
+
+    transform.setOrigin( tf::Vector3(goal_x , goal_y , 0.0) );
+    quaternion.setRPY(0, 0, goal_theta);
+    transform.setRotation(quaternion);
+    broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "final_goal_point"));
+
+    transform.setOrigin( tf::Vector3(0.2 , 0.2 , 0.0) );
+    quaternion.setRPY(0, 0, 1);
+    transform.setRotation(quaternion);
+    broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "test2"));
+
+    ros::spinOnce();
+    rate.sleep();
+  }
+
   return 0;
 }
+
+//#include <ros/ros.h>
+//#include <tf/transform_broadcaster.h>
+
+//int main(int argc, char** argv){
+//  ros::init(argc, argv, "my_tf_broadcaster");
+//  ros::NodeHandle node;
+
+//  tf::TransformBroadcaster br;
+//  tf::Transform transform;
+
+//  ros::Rate rate(10.0);
+//  while (node.ok()){
+//    transform.setOrigin( tf::Vector3(0.0, 2.0, 0.0) );
+//    transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
+//    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "turtle1", "carrot1"));
+//    rate.sleep();
+//  }
+//  return 0;
+//}
