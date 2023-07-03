4a7d04e690a35c6728b31a652f87a3e6db4c41a8
kijonggil <kkjong@robotis.com>
2023-07-03 18:00:13 +0900

    Modified quat2euler
    Signed-off-by: kijonggil <kkjong@robotis.com>

--- turtlebot3_automatic_parking/turtlebot3_automatic_parking/turtlebot3_automatic_parking.py
+++ turtlebot3_automatic_parking/turtlebot3_automatic_parking/turtlebot3_automatic_parking.py
@@ -30,7 +30,7 @@ from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Empty
 
 import numpy as np
-import tf_transformations
+import transforms3d
 
 class AutomaticParking(Node):
 
@@ -104,7 +104,7 @@ class AutomaticParking(Node):
             orientation.w
         )
 
-        self.euler = tf_transformations.quaternion_to_euler(quaternion)
+        self.euler = transforms3d.quat2euler(quaternion)
 
     def _get_point(self, start_angle_distance):
         angle = start_angle_distance[0]
