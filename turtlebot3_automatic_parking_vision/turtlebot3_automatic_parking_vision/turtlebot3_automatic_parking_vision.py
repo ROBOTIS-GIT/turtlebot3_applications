d5889dc53af013d27b4c5176c354450e457c5e24
kijonggil <kkjong@robotis.com>
2023-07-11 20:11:04 +0900

    Added pose
    Signed-off-by: kijonggil <kkjong@robotis.com>

--- turtlebot3_automatic_parking_vision/turtlebot3_automatic_parking_vision/turtlebot3_automatic_parking_vision.py
+++ turtlebot3_automatic_parking_vision/turtlebot3_automatic_parking_vision/turtlebot3_automatic_parking_vision.py
@@ -26,9 +26,11 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose
 from nav_msgs.msg import Odometry
 from ros2_aruco_interfaces.msg import ArucoMarkers
 
+from tf_transformations import euler_from_quaternion
 import numpy as np
 
 MARKER_ID_DETECTION = 17
@@ -113,13 +115,70 @@ class AutomaticParkingVision(Node):
         if not self.is_marker_received:
             for i in range(len(msg.marker_ids)):
                 if msg.marker_ids[i] == MARKER_ID_DETECTION:
-                    if self.is_marker_pose_received == False:
-                        self.is_marker_pose_received = True
-
-                    pos_x, pos_y, theta = self.fnGet2DMarkerPose(msg.poses[i])
+                    pos_x, pos_y, theta = self._get_marker_pose(msg.poses[i])
                     self._set_goal_position(pos_x, pos_y, theta - math.pi)
                     self.get_logger().info('marker detected {} {} {}'.format(pos_x, pos_y, theta))
                     self.is_marker_received = True
+
+    def _rotate_odom(self, odom):
+        self.get_logger().info("odom {0}".format(odom.position))
+        rotation_x = math.pi / 2
+        cos_angle_x = math.cos(rotation_x)
+        sin_angle_x = math.sin(rotation_x)
+        rotation_matrix_x = [[1, 0, 0],
+                            [0, cos_angle_x, -sin_angle_x],
+                            [0, sin_angle_x, cos_angle_x]]
+
+        rotation_z = math.pi / 2
+        cos_angle_z = math.cos(rotation_z)
+        sin_angle_z = math.sin(rotation_z)
+        rotation_matrix_z = [[cos_angle_z, -sin_angle_z, 0],
+                            [sin_angle_z, cos_angle_z, 0],
+                            [0, 0, 1]]
+
+        pose_matrix = [[odom.position.x],
+                    [odom.position.y],
+                    [odom.position.z]]
+        rotated_odom_matrix = np.dot(rotation_matrix_z, np.dot(rotation_matrix_x, pose_matrix))
+
+        orientation = [odom.orientation.x, odom.orientation.y, odom.orientation.z, odom.orientation.w]
+        rotated_orientation = [0, 0, 0, 0]
+        rotated_orientation[0] = cos_angle_x * cos_angle_z * orientation[0] - sin_angle_x * sin_angle_z * orientation[1]
+        rotated_orientation[1] = sin_angle_x * cos_angle_z * orientation[0] + cos_angle_x * sin_angle_z * orientation[1]
+        rotated_orientation[2] = cos_angle_x * sin_angle_z * orientation[0] + sin_angle_x * cos_angle_z * orientation[2]
+        rotated_orientation[3] = cos_angle_x * cos_angle_z * orientation[3] - sin_angle_x * sin_angle_z * orientation[3]
+
+        rotated_odom = Pose()
+        rotated_odom.position.x = rotated_odom_matrix[0][0]
+        rotated_odom.position.y = -rotated_odom_matrix[1][0]
+        rotated_odom.position.z = rotated_odom_matrix[2][0]
+        rotated_odom.orientation.x = rotated_orientation[0]
+        rotated_odom.orientation.y = rotated_orientation[1]
+        rotated_odom.orientation.z = rotated_orientation[2]
+        rotated_odom.orientation.w = rotated_orientation[3]
+
+        return rotated_odom
+
+    def _get_marker_pose(self, marker_pose):
+            pose = self._rotate_pose(marker_pose)
+            self.get_logger().info("rotation pose {0}".format(pose.position))
+            quaternion = (
+                pose.orientation.x,
+                pose.orientation.y,
+                pose.orientation.z,
+                pose.orientation.w)
+            theta = euler_from_quaternion(quaternion)[2]
+            theta = theta + np.pi / 2.
+
+            if theta < 0.0:
+                theta = theta + np.pi * 2
+            if theta > np.pi * 2:
+                theta = theta - np.pi * 2
+
+            pos_x = pose.position.x
+            pos_y = pose.position.y
+
+            return pos_x, pos_y, theta
 
     def _set_goal_position(self, x, y, theta):
         self.goal_position.x = x
