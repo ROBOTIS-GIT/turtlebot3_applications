2d30e79058a54b4d160d456205b39f63b1ef24f0
kijonggil <kkjong@robotis.com>
2023-07-12 19:02:48 +0900

    Added launch
    Signed-off-by: kijonggil <kkjong@robotis.com>

--- turtlebot3_follower/launch/turtlebot3_follower.launch.py
+++ turtlebot3_follower/launch/turtlebot3_follower.launch.py
@@ -2,12 +2,29 @@
 # Copyright 2023 ROBOTIS CO., LTD.
 # Authors: Gilbert
 
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    filter_param = os.path.join(
+                get_package_share_directory('turtlebot3_follower'),
+                'filter',
+                'turtlebot3_follow_filter.yaml')
     return LaunchDescription([
+        Node(
+            package='laser_filters',
+            executable='scan_to_scan_filter_chain',
+            name='scan_to_scan_filter_chain',
+            output='screen',
+            parameters=[
+                {'params-file': filter_param},
+            ],
+        ),
+
         Node(
             package='turtlebot3_follower',
             executable='turtlebot3_follower',
