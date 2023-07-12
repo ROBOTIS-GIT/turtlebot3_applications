7a4a03dc9b7e74a4e3c3989116df00e856952004
kijonggil <kkjong@robotis.com>
2023-07-12 13:34:44 +0900

    Modifie ueler
    Signed-off-by: kijonggil <kkjong@robotis.com>

--- turtlebot3_automatic_parking_vision/turtlebot3_automatic_parking_vision/turtlebot3_automatic_parking_vision.py
+++ turtlebot3_automatic_parking_vision/turtlebot3_automatic_parking_vision/turtlebot3_automatic_parking_vision.py
@@ -207,7 +207,7 @@ class AutomaticParkingVision(Node):
                 pose.orientation.y,
                 pose.orientation.z,
                 pose.orientation.w)
-            theta = euler_from_quaternion(quaternion)[0]
+            theta = euler_from_quaternion(quaternion)[2]
             # theta = theta + np.pi / 2.
 
             # if theta < 0.0:
