d00f4a2c37ba256c07ce8d84c7bdf657d9ad325c
kijonggil <kkjong@robotis.com>
2023-08-30 19:34:42 +0900

    Added camera launch
    Signed-off-by: kijonggil <kkjong@robotis.com>

--- turtlebot3_panorama/src/panorama.cpp
+++ turtlebot3_panorama/src/panorama.cpp
@@ -74,7 +74,6 @@ void turtlebot3_panorama::PanoApp::setup()
   it_ = std::make_unique<image_transport::ImageTransport>(shared_from_this());
 
   pub_stitched = it_->advertise("/panorama", 1);
-  // sub_camera = it_->subscribe("/image_raw/compressed", 1, &PanoApp::cameraImageCb, this);
 
   sub_camera = this->create_subscription<sensor_msgs::msg::CompressedImage>(
     "/image_raw/compressed",
