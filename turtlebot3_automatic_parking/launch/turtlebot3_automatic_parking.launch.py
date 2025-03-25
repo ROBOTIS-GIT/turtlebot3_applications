6322e4dabad4d9d8e94a2fbe7e9a4523d451aa34
nyeonsoo <nys827827@gmail.com>
2025-03-25 09:42:24 +0900

    edited copyright year

--- turtlebot3_automatic_parking/launch/turtlebot3_automatic_parking.launch.py
+++ turtlebot3_automatic_parking/launch/turtlebot3_automatic_parking.launch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #################################################################################
-# Copyright 2025 ROBOTIS CO., LTD.
+# Copyright 2023 ROBOTIS CO., LTD.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
