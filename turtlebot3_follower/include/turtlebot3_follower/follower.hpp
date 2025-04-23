406a6728f847f390bf69f0a73946e4494a8bcaf3
Hyungyu Kim <kimhg@robotis.com>
2025-04-23 14:15:03 +0900

    Add velocity smoother
    Signed-off-by: Hyungyu Kim <kimhg@robotis.com>

--- turtlebot3_follower/include/turtlebot3_follower/follower.hpp
+++ turtlebot3_follower/include/turtlebot3_follower/follower.hpp
@@ -1,57 +1,64 @@
-#ifndef FOLLOWER_HPP
-#define FOLLOWER_HPP
+// Copyright 2019 ROBOTIS CO., LTD.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Hyungyu Kim
+
+#ifndef TURTLEBOT3_FOLLOWER__FOLLOWER_HPP_
+#define TURTLEBOT3_FOLLOWER__FOLLOWER_HPP_
+
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
 
 #include <memory>
 #include <chrono>
 #include <string>
 
-#include "rclcpp/rclcpp.hpp"
-// #include "geometry_msgs/msg/pose_stamped.hpp"
-// #include "nav2_msgs/action/navigate_to_pose.hpp"
-// #include "rclcpp_action/rclcpp_action.hpp"
-// #include "tf2_ros/buffer.h"
-// #include "tf2_ros/transform_listener.h"
-#include "geometry_msgs/msg/transform_stamped.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-#include "tf2_ros/transform_broadcaster.h"
-#include "tf2_ros/static_transform_broadcaster.h"
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-#include "tf2_ros/transform_listener.h"
-#include "tf2_ros/buffer.h"
-#include "geometry_msgs/msg/transform_stamped.hpp"
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <nav_msgs/msg/path.hpp>
 #include <nav2_msgs/action/navigate_to_pose.hpp>
+#include <nav2_msgs/action/follow_path.hpp>
+#include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "nav_msgs/msg/path.hpp"
-#include "nav2_msgs/action/follow_path.hpp"
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
-class Follower : public rclcpp::Node {
+
+class Follower : public rclcpp::Node
+{
 public:
-  // using NavigateToPose = nav2_msgs::action::NavigateToPose;
   explicit Follower(const std::string & node_name, const std::string & leader_name);
 
 private:
   void tf_publisher();
-  void send_goal();
-  void coordinate_transform();
-  geometry_msgs::msg::Quaternion get_rotation(const nav_msgs::msg::Odometry::SharedPtr leader_odom_msg,
-    const nav_msgs::msg::Odometry::SharedPtr follower_odom_msg);
+  void send_path();
+  void get_target_pose();
+
   geometry_msgs::msg::TransformStamped target_pose_;
-  geometry_msgs::msg::PoseStamped prior_target_pose_;
+  geometry_msgs::msg::PoseStamped prior_second_target_pose_;
+  rclcpp::TimerBase::SharedPtr send_path_timer_;
+  rclcpp_action::Client<nav2_msgs::action::FollowPath>::SharedPtr nav2_action_client_;
   std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_broadcaster_;
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::TimerBase::SharedPtr timer2_;
-  rclcpp_action::Client<nav2_msgs::action::FollowPath>::SharedPtr client_;
+
   std::string leader_name_;
   std::string follower_name_;
-  std::string leader_odom_;
-  std::string follower_odom_;
-  double follow_distance_;
-  double goal_interval_;
   bool use_sim_time_;
 };
 
-#endif // FOLLOWER_HPP
+#endif  // TURTLEBOT3_FOLLOWER__FOLLOWER_HPP_
--- turtlebot3_follower/src/follower.cpp
+++ turtlebot3_follower/src/follower.cpp
@@ -1,149 +1,120 @@
-// follower_node.cpp
+// Copyright 2019 ROBOTIS CO., LTD.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Hyungyu Kim
+
 #include "turtlebot3_follower/follower.hpp"
 
-using namespace std::chrono_literals;
 
 Follower::Follower(const std::string & follower_name, const std::string & leader_name)
-: Node(follower_name+"_follower_node"),
+: Node(follower_name + "_follower_node"),
   tf_buffer_(this->get_clock()),
   tf_listener_(tf_buffer_),
   leader_name_(leader_name),
-  follower_name_(follower_name),
-  leader_odom_(leader_name+"/odom"),
-  follower_odom_(follower_name+"/odom"),
-  follow_distance_(0.15),
-  goal_interval_(1.0)
+  follower_name_(follower_name)
 {
-  this->get_parameter("use_sim_time", use_sim_time_);
+  get_parameter("use_sim_time", use_sim_time_);
+
   tf_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
-  client_ = rclcpp_action::create_client<nav2_msgs::action::FollowPath>(this, follower_name_+"/follow_path");
-  RCLCPP_INFO(this->get_logger(), "Follower node initialized with NodeOptions");
+  nav2_action_client_ = rclcpp_action::create_client<nav2_msgs::action::FollowPath>(
+    this,
+    follower_name_ + "/follow_path");
+
   tf_publisher();
-  timer_ = this->create_wall_timer(
+  send_path_timer_ = this->create_wall_timer(
     std::chrono::milliseconds(100),
-    std::bind(&Follower::coordinate_transform, this)
-  );
-  timer2_ = this->create_wall_timer(
-    std::chrono::milliseconds(1000),
-    std::bind(&Follower::send_goal, this)
-  );
+    std::bind(&Follower::send_path, this));
+
+  RCLCPP_INFO(
+    this->get_logger(),
+    "[%s] initialized successfully",
+    this->get_name());
 }
 
-void Follower::send_goal() {
-  auto goal_msg = nav2_msgs::action::FollowPath::Goal();
-  geometry_msgs::msg::PoseStamped pose;
-  nav_msgs::msg::Path path;
-  // 이전 목표 위치를 경로에 추가
-  pose.header.stamp = this->get_clock()->now();
-  pose.header.frame_id = follower_name_+"/base_link";
-  pose.pose.position.x = this->prior_target_pose_.pose.position.x;
-  pose.pose.position.y = this->prior_target_pose_.pose.position.y;
-  pose.pose.orientation = this->prior_target_pose_.pose.orientation;
-  path.poses.push_back(pose);
-  // 현재 목표 위치를 경로에 추가
-  geometry_msgs::msg::PoseStamped pose2;
-  pose2.header.stamp = this->get_clock()->now();
-  pose2.header.frame_id = follower_name_+"/base_link";
-  pose2.pose.position.x = this->target_pose_.transform.translation.x;
-  pose2.pose.position.y = this->target_pose_.transform.translation.y;
-  pose2.pose.orientation.w = 1.0;
-  //골을 쏠 때마다 이전 목표 위치를 저장
-  if (pose!= pose2){
-    this->prior_target_pose_= pose2;
-    this->prior_target_pose_.pose.orientation =this->target_pose_.transform.rotation;
-  }
-  path.poses.push_back(pose2);
-
-  path.header.stamp = this->get_clock()->now();
-  path.header.frame_id = follower_name_+"/base_link";
-  goal_msg.path = path;
-
-  if (!this->client_->wait_for_action_server(1s)) {
-    RCLCPP_WARN(this->get_logger(), "Action server not available");
-    return;
-  }
-  // RCLCPP_INFO(this->get_logger(),
-  // "Sending goal to action server, target_x: [%.2f], target_y: [%.2f]",
-  // this->transformStamped.transform.translation.x,
-  // this->transformStamped.transform.translation.y);
-  this->client_->async_send_goal(goal_msg);
-}
-
-void Follower::tf_publisher() {
-  // auto& leader_odom_msg = this->leader_odom_msg_;
-  // auto& follower_odom_msg = this->follower_odom_msg_;
-
-  // while (){
-  //   RCLCPP_INFO(this->get_logger(), "Waiting for odometry messages...");
-  //   std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  // }
-
+void Follower::tf_publisher()
+{
   geometry_msgs::msg::TransformStamped tf_msg;
   tf_msg.header.stamp = this->get_clock()->now();
-  tf_msg.header.frame_id = this->leader_odom_;
-  tf_msg.child_frame_id = this->follower_odom_;
+  tf_msg.header.frame_id = this->leader_name_ + "/odom";
+  tf_msg.child_frame_id = this->follower_name_ + "/odom";
 
-  if(this->use_sim_time_ == true){
+  if (this->use_sim_time_ == true) {
     tf_msg.transform.translation.x = 0;
     tf_msg.transform.translation.y = 0;
     tf_msg.transform.translation.z = 0;
     tf_msg.transform.rotation.y = 0.0;
     tf_msg.transform.rotation.z = 0.0;
     tf_msg.transform.rotation.w = 1.0;
-  } else{
-    tf_msg.transform.translation.x = -0.14;//follower_odom_msg->pose.pose.position.x - leader_odom_msg->pose.pose.position.x;
-    tf_msg.transform.translation.y = 0;//follower_odom_msg->pose.pose.position.y - leader_odom_msg->pose.pose.position.y;
-    tf_msg.transform.translation.z = 0;//follower_odom_msg->pose.pose.position.z - leader_odom_msg->pose.pose.position.z;
+  } else {
+    tf_msg.transform.translation.x = -0.14;
+    tf_msg.transform.translation.y = 0;
+    tf_msg.transform.translation.z = 0;
     tf_msg.transform.rotation.x = 0.0;
     tf_msg.transform.rotation.y = 0.0;
     tf_msg.transform.rotation.z = 0.0;
     tf_msg.transform.rotation.w = 1.0;
   }
-  RCLCPP_INFO(this->get_logger(),"tf publish@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+  RCLCPP_INFO(this->get_logger(), "tf publish");
   this->tf_broadcaster_->sendTransform(tf_msg);
-
 }
 
-void Follower::coordinate_transform()
+void Follower::get_target_pose()
 {
   try {
-    this->target_pose_ =
-      this->tf_buffer_.lookupTransform(
-        this->follower_name_+"/base_link",
-        this->leader_name_+"/base_link",
-        tf2::TimePointZero);  // 가장 최근의 Transform
-
-    // RCLCPP_INFO(this->get_logger(),
-    //   "Transform: %s -> %s \n"
-    //   "Translation: [%.2f, %.2f, %.2f]\n"
-    //   "Rotation (quaternion): [x: %.2f, y: %.2f, z: %.2f, w: %.2f]",
-    //   (this->leader_name_+"/base_link").c_str(),
-    //   (this->follower_name_+"/base_link").c_str(),
-    //   this->target_pose_.transform.translation.x,
-    //   this->target_pose_.transform.translation.y,
-    //   this->target_pose_.transform.translation.z,
-    //   this->target_pose_.transform.rotation.x,
-    //   this->target_pose_.transform.rotation.y,
-    //   this->target_pose_.transform.rotation.z,
-    //   this->target_pose_.transform.rotation.w
-    // );
-  } catch (const tf2::TransformException &ex) {
-    RCLCPP_WARN(this->get_logger(), "Could not fine TF: %s", ex.what());
+    this->target_pose_ = this->tf_buffer_.lookupTransform(
+      this->follower_name_ + "/base_link",
+      this->leader_name_ + "/base_link",
+      tf2::TimePointZero);
+  } catch (const tf2::TransformException & ex) {
+    RCLCPP_WARN(this->get_logger(), "Waiting for TF");
   }
 }
 
-geometry_msgs::msg::Quaternion Follower::get_rotation(const nav_msgs::msg::Odometry::SharedPtr leader_odom_msg,
-  const nav_msgs::msg::Odometry::SharedPtr follower_odom_msg)
+void Follower::send_path()
 {
-geometry_msgs::msg::Quaternion leader_q = leader_odom_msg->pose.pose.orientation;
-geometry_msgs::msg::Quaternion follower_q = follower_odom_msg->pose.pose.orientation;
+  get_target_pose();
+  nav_msgs::msg::Path path;
+  path.header.stamp = this->get_clock()->now();
+  path.header.frame_id = follower_name_ + "/base_link";
 
-tf2::Quaternion tf_leader_q, tf_follower_q;
-tf2::fromMsg(leader_q, tf_leader_q);
-tf2::fromMsg(follower_q, tf_follower_q);
-tf2::Quaternion relative_q = tf_follower_q * tf_leader_q.inverse();  // follower - leader
+  geometry_msgs::msg::PoseStamped first_target_pose;
+  first_target_pose.header.stamp = this->get_clock()->now();
+  first_target_pose.header.frame_id = follower_name_ + "/base_link";
+  first_target_pose.pose.position.x = this->prior_second_target_pose_.pose.position.x;
+  first_target_pose.pose.position.y = this->prior_second_target_pose_.pose.position.y;
+  first_target_pose.pose.orientation = this->prior_second_target_pose_.pose.orientation;
+  path.poses.push_back(first_target_pose);
 
-return tf2::toMsg(relative_q);
+  geometry_msgs::msg::PoseStamped second_target_pose;
+  second_target_pose.header.stamp = this->get_clock()->now();
+  second_target_pose.header.frame_id = follower_name_ + "/base_link";
+  second_target_pose.pose.position.x = this->target_pose_.transform.translation.x;
+  second_target_pose.pose.position.y = this->target_pose_.transform.translation.y;
+  second_target_pose.pose.orientation.w = 1.0;
+  path.poses.push_back(second_target_pose);
+  if (first_target_pose != second_target_pose) {
+    this->prior_second_target_pose_ = second_target_pose;
+  }
+
+  auto goal_msg = nav2_msgs::action::FollowPath::Goal();
+  goal_msg.path = path;
+
+  if (!this->nav2_action_client_->wait_for_action_server(std::chrono::seconds(1))) {
+    RCLCPP_WARN(this->get_logger(), "Action server not available");
+    return;
+  }
+  this->nav2_action_client_->async_send_goal(goal_msg);
 }
 
 int main(int argc, char * argv[])
@@ -151,7 +122,6 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
 
   int number = 0;
-
   if (argc > 1) {
     try {
       number = std::stoi(argv[1]);
@@ -164,13 +134,13 @@ int main(int argc, char * argv[])
   std::vector<std::shared_ptr<Follower>> followers;
   for (int i = 1; i <= number; ++i) {
     auto node = std::make_shared<Follower>(
-      "TB3_"/*"follower_*/ + std::to_string(i+1),
-      "TB3_"/*"leader_*/ + std::to_string(i));
+      "TB3_" + std::to_string(i + 1),
+      "TB3_" + std::to_string(i));
     followers.push_back(node);
   }
 
   rclcpp::executors::MultiThreadedExecutor executor;
-  for (auto& node : followers) {
+  for (auto & node : followers) {
     executor.add_node(node);
   }
 
