adc554b88b9cddab5f82800fb5c8571043080ae5
Hyungyu Kim <kimhg@robotis.com>
2025-06-20 14:16:35 +0900

    bump
    Signed-off-by: Hyungyu Kim <kimhg@robotis.com>

--- turtlebot3_aruco_tracker/setup.py
+++ turtlebot3_aruco_tracker/setup.py
@@ -8,7 +8,7 @@ share_dir = 'share/' + package_name
 
 setup(
     name=package_name,
-    version='1.3.0',
+    version='1.3.1',
     packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
